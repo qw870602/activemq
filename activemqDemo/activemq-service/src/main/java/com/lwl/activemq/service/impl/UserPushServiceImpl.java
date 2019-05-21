6d8a5d637d11fc8d5c0b6a90611f3fbeadf82e65
qw870602 <fengxiao1993@yeah.net>
2019-05-21 23:21:45 +0800

    Add files via upload

---
+++ activemqDemo/activemq-service/src/main/java/com/lwl/activemq/service/impl/UserPushServiceImpl.java
@@ -0,0 +1,48 @@
+package com.lwl.activemq.service.impl;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import com.alibaba.fastjson.JSON;
+import com.lwl.activemq.domain.User;
+import com.lwl.activemq.service.PushService;
+
+
+@Service("userPushService")
+public class UserPushServiceImpl implements PushService {
+
+	
+	@Autowired
+	private JmsTemplate jmsTemplate;
+	
+	/**
+	 * 这里是根据MQ配置文件定义的queue来注入的，也就是这里将会把不同的内容推送到不同的queue中
+	 */
+	@Autowired
+	@Qualifier("userServiceQueue")
+	private Destination destination;
+	
+	@Override
+	public void push(final Object info) {
+		pushExecutor.execute(new Runnable() {
+			@Override
+			public void run() {
+				jmsTemplate.send(destination, new MessageCreator() {
+					public Message createMessage(Session session) throws JMSException {
+						 User p = (User) info;
+						return session.createTextMessage(JSON.toJSONString(p));
+					}
+				});
+			}			
+		});
+	}
+
+}
